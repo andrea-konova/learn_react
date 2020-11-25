557308f374e06620bdba3112c04f6c10ab50357a
Andrii Konovalenko <an256.ak@gmail.com>
2020-11-25 15:34:37 +0200

    feat: created the project structure

---
+++ first-project/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='main-content'>
+      <div>
+        <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
+      </div>
+      <div>
+        ava + desc
+        </div>
+      <div>
+        my-post
+          <div>
+          new post
+          </div>
+        <div>
+          post-1
+          </div>
+        <div>
+          post-2
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
