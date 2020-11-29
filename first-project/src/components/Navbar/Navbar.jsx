53b92193e1047b65ac39a5e0e03e305858cf45d0
Andrii Konovalenko <an256.ak@gmail.com>
2020-11-30 01:44:15 +0200

    feat: added NavLink in Navbar

--- first-project/src/components/Navbar/Navbar.jsx
+++ first-project/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <nav className={classes.navbar}>
-        <ul className={classes.menu}>
-          <li className={classes.list}>
-            <a href='/profile' className={`${classes.item} ${classes.active}`}>Profile</a>
-          </li>
-          <li className={classes.list}>
-            <a href='/dialogs' className={classes.item}>Messages</a>
-          </li>
-          <li className={classes.list}>
-            <a href='/news' className={classes.item}>News</a>
-          </li>
-          <li className={classes.list}>
-            <a href='/music' className={classes.item}>Music</a>
-          </li>
-          <li className={`${classes.list} ${classes.mt}`}>
-            <a href='/settings' className={classes.item}>Settings</a>
-          </li>
-        </ul>
-      </nav>
-  );
+  return <nav className={classes.navbar}>
+      <ul className={classes.menu}>
+        <li className={classes.list}>
+          <NavLink to='/profile' className={classes.item} activeClassName={classes.active}>Profile</NavLink>
+        </li>
+        <li className={classes.list}>
+          <NavLink to='/dialogs' className={classes.item} activeClassName={classes.active}>Messages</NavLink>
+        </li>
+        <li className={classes.list}>
+          <NavLink to='/news' className={classes.item} activeClassName={classes.active}>News</NavLink>
+        </li>
+        <li className={classes.list}>
+          <NavLink to='/music' className={classes.item} activeClassName={classes.active}>Music</NavLink>
+        </li>
+        <li className={`${classes.list} ${classes.mt}`}>
+          <NavLink to='/settings' className={classes.item} activeClassName={classes.active}>Settings</NavLink>
+        </li>
+      </ul>
+    </nav>;
 }
 
 export default Navbar;
