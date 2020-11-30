336d5916ef3475d269af5a3544bc2e6c1d835e3b
Andrii Konovalenko <an256.ak@gmail.com>
2020-11-30 02:44:29 +0200

    feat: added dialogs anв messages

--- first-project/src/components/Dialogs/Dialogs.jsx
+++ first-project/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={classes.dialogs}>
+      <div className={classes.dialogs__items}>
+        <div className={classes.dialog}>
+          Dimych
+        </div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          Andrey
+        </div>
+        <div className={classes.dialog}>
+          Ania
+        </div>
+        <div className={classes.dialog}>
+          Tima
+        </div>
+        <div className={classes.dialog}>
+          Sasha
+        </div>
+        <div className={classes.dialog}>
+          Vika
+        </div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>What you doing?</div>
+        <div className={classes.message}>I'm fine!</div>
+      </div>
     </div>
   );
 }
