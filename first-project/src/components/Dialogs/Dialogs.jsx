e4caaf11301779e38b209d6dd71a83434d9e3acf
Andrii Konovalenko <an256.ak@gmail.com>
2020-12-01 23:25:45 +0200

    fix: refactored code

--- first-project/src/components/Dialogs/Dialogs.jsx
+++ first-project/src/components/Dialogs/Dialogs.jsx
@@ -4,20 +4,34 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
+
+  let dialogsData = [
+    {id: 1, name: 'Dimych'},
+    {id: 2, name: 'Andrew'},
+    {id: 3, name: 'Anna'},
+    {id: 4, name: 'Tima'},
+    {id: 5, name: 'Sasha'},
+    {id: 6, name: 'Vika'}
+  ];
+
+  let messagesData = [
+    {id: 1, message: 'HI'},
+    {id: 2, message: 'What you doing?'},
+    {id: 3, message: 'I am fine!'},
+    {id: 4, message: 'Good bay'}
+  ];
+
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogs__items}>
-        <DialogItem name='Dimych' id='1' />
-        <DialogItem name='Andrey' id='2' />
-        <DialogItem name='Ania' id='3' />
-        <DialogItem name='Tima' id='4' />
-        <DialogItem name='Sasha' id='5' />
-        <DialogItem name='Vika' id='6' />
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
       </div>
       <div className={classes.messages}>
-        <Message message='Hello' />
-        <Message message='What you doing?' />
-        <Message message='I am fine!' />
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
+        <Message message={messagesData[2].message} />
+        <Message message={messagesData[3].message} />
       </div>
     </div>
   );
--- first-project/src/components/Profile/MyPosts/MyPosts.jsx
+++ first-project/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,11 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+  let postsData = [
+    {id: 1, message: 'We bomb the ReactJS at the rate of Dimych!', likeCount: '15'},
+    {id: 2, message: 'Hello! I am learning ReactJS!', likeCount: '20'}
+  ];
+
   return (
     <div className={classes.posts}>
       <h2 className={classes.name}>My Post</h2>
@@ -10,8 +15,8 @@ const MyPosts = () => {
         <textarea autoComplete='off' rows='5' name='message' className={classes.new__post} id='message' placeholder='your news...'></textarea>
         <button className={classes.button}>Send</button>
       </div>
-      <Post message='We bomb the ReactJS at the rate of Dimych!' likeCount='15' />
-      <Post message='Hello! I am learning ReactJS!' likeCount='20' />
+      <Post message={postsData[0].message} likeCount={postsData[0].likeCount} />
+      <Post message={postsData[1].message} likeCount={postsData[1].likeCount} />
     </div>
   );
 }
