252c323b63151858d4f3332c790683d0f92ac753
Andrii Konovalenko <an256.ak@gmail.com>
2021-02-12 22:38:36 +0200

    feat: added a request to the server

--- first-project/src/components/Users/Users.jsx
+++ first-project/src/components/Users/Users.jsx
@@ -1,52 +1,22 @@
 import React from 'react';
 import classes from './Users.module.css';
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png";
 
 const Users = (props) => {
-  debugger
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl: 'https://www.meme-arsenal.com/memes/b877babd9c07f94b952c7f152c4e264e.jpg',
-        followed: true,
-        fullName: 'Dimych',
-        status: 'He is big boss',
-        location: {city: 'Minsk', country: 'Belarus'}
-      },
-      {
-        id: 2,
-        photoUrl: 'https://www.meme-arsenal.com/memes/b877babd9c07f94b952c7f152c4e264e.jpg',
-        followed: false,
-        fullName: 'Andrew',
-        status: 'I\'m student' ,
-        location: {city: 'Kharkiv', country: 'Ukraine'}
-      },
-      {
-        id: 3,
-        photoUrl: 'https://www.meme-arsenal.com/memes/b877babd9c07f94b952c7f152c4e264e.jpg',
-        followed: true,
-        fullName: 'Anna',
-        status: 'I am Andrey\'s wife ' ,
-        location: {city: 'Kharkiv', country: 'Ukraine'}
-      },
-      {
-        id: 4,
-        photoUrl: 'https://www.meme-arsenal.com/memes/b877babd9c07f94b952c7f152c4e264e.jpg',
-        followed: true,
-        fullName: 'Tima',
-        status: 'I\'m a little javascript programmer' ,
-        location: {city: 'Kharkiv', country: 'Ukraine'}
-      }
-    ])
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      props.setUsers(response.data.items)
+    });
+
   }
-  debugger
   return (
     <div>
       {
         props.users.map( u => <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={classes.userPhoto} />
+              <img src={ u.photos.small != null ? u.photos.small : userPhoto } className={classes.userPhoto} />
             </div>
             <div>
               { u.followed
@@ -57,12 +27,12 @@ const Users = (props) => {
           </span>
           <span>
             <div>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </div>
             <div>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </div>
           </span>
 
