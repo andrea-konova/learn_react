bf6c7b58b5af2a9cbef5afcddc36ff6137439c57
Andrii Konovalenko <an256.ak@gmail.com>
2021-02-21 21:22:33 +0200

    feat: added users pagination

--- first-project/src/redux/users-reducer.js
+++ first-project/src/redux/users-reducer.js
@@ -1,11 +1,16 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_CURRENT = 'SET_TOTAL_USERS_CURRENT';
 
 let initialState = {
   users: [
 
-  ]
+  ],
+    pageSize: 100,
+    totalUsersCount: 0,
+    currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -33,7 +38,15 @@ const usersReducer = (state = initialState, action) => {
     case SET_USERS:
       return {
         ...state,
-        users: [ ...state.users, ...action.users ]
+        users: action.users
+      };
+    case SET_CURRENT_PAGE:
+      return {
+        ...state, currentPage: action.currentPage
+      };
+    case SET_TOTAL_USERS_CURRENT:
+      return {
+        ...state, totalUsersCount: action.totalUsersCount
       };
     default:
       return state;
@@ -54,10 +67,24 @@ export const unfollowAC = (userId) => {
   }
 };
 
-export const setUsersAC= (users) => {
+export const setUsersAC = (users) => {
   return {
     type: SET_USERS,
     users
+  }
+};
+
+export const setCurrentPageAC = (currentPage) => {
+  return {
+    type: SET_CURRENT_PAGE,
+    currentPage
+  }
+};
+
+export const setTotalUsersCountAC = (totalUsersCount) => {
+  return {
+    type: SET_TOTAL_USERS_CURRENT,
+    totalUsersCount
   }
 };
 
