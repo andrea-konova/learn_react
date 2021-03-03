f8cd5a191b18eff98951b567ef2e903d4efe99a3
Andrii Konovalenko <an256.ak@gmail.com>
2021-03-03 20:13:58 +0200

    feat: added login

--- first-project/src/components/Header/Header.jsx
+++ first-project/src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={classes.header}>
-      <span className={classes.logo}>vReacte</span>
+      <div className={classes.headRow}>
+        <span className={classes.logo}>vReacte</span>
+        <div className={classes.loginBlock}>
+          {props.isAuth ? props.login
+            : <NavLink to={'/login'}>Login</NavLink>  }
+        </div>
+      </div>
     </header>
   );
 }
